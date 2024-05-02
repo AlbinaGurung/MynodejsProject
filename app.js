8dca7c95a7d35a9c45ef194be773ab851e9df258
Albina Gurung <albinakushu9545@gmail.com>
2024-05-03 02:44:57 +0545

    First commit

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ console.log("Server is running")
 const express=require("express")
 const app=express();
 
-app.get('/home',(request,response)=>{
+app.get('/',(request,response)=>{
 response.send("successfully hosted my nodejs appplication on browser.")
 });
+app.get('/Home',(request,response)=>
+{
+response.render('home.ejs')
+});
+app.get('/About',(request,response)=>{
+    response.render('about.ejs')
+})
 
 app.listen(3000,"127.0.0.1");
 //Our project will listen to port 3000 i.e port no 3000 will be given to 
